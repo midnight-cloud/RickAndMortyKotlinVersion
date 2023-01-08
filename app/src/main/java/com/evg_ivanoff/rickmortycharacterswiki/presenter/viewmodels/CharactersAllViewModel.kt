8f052b79518136009278cc79e8997c1afa389ae3
Eugene Ivanov <wied11@yandex.ru>
2023-01-09 02:39:11 +0500

    add paging3 realization delete buttons to next and prev page changes in CharactersAllViewModel.kt to apply paging

--- app/src/main/java/com/evg_ivanoff/rickmortycharacterswiki/presenter/viewmodels/CharactersAllViewModel.kt
+++ app/src/main/java/com/evg_ivanoff/rickmortycharacterswiki/presenter/viewmodels/CharactersAllViewModel.kt
@@ -2,62 +2,36 @@ package com.evg_ivanoff.rickmortycharacterswiki.presenter.viewmodels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import com.evg_ivanoff.rickmortycharacterswiki.data.api.ApiFactory
-import com.evg_ivanoff.rickmortycharacterswiki.data.repository.CharacterRepoImpl
-import com.evg_ivanoff.rickmortycharacterswiki.domain.models.MainCharsModel
-import com.evg_ivanoff.rickmortycharacterswiki.domain.usecases.GetCharactersListByPageUsecase
-import com.evg_ivanoff.rickmortycharacterswiki.domain.usecases.GetCharactersListUsecase
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.evg_ivanoff.rickmortycharacterswiki.data.api.ApiPageSource
+import com.evg_ivanoff.rickmortycharacterswiki.domain.models.CharacterModel
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.stateIn
 
 
-class CharactersAllViewModel(
-    private val getCharactersListUsecase: GetCharactersListUsecase,
-    private val getCharactersListByPageUsecase: GetCharactersListByPageUsecase
-) : ViewModel() {
+class CharactersAllViewModel() : ViewModel() {
 
+    private var _liveData: StateFlow<PagingData<CharacterModel>> = Pager(
+        PagingConfig(pageSize = 3)
+    ) {
+        ApiPageSource()
+    }.flow
+        .cachedIn(viewModelScope)   // for recreate activity (orientation change)
+        .stateIn(viewModelScope, SharingStarted.Lazily, PagingData.empty())
 
-    private val _liveData = MutableStateFlow(MainCharsModel(null, null))
-    val liveData: StateFlow<MainCharsModel> = _liveData.asStateFlow()
-    //asStateFlow() для readonly
-
-    init {
-        loadData()
-    }
-
-    private fun loadData() {
-        CoroutineScope(Dispatchers.IO).launch {
-            _liveData.value = getCharactersListUsecase.execute()
-        }
-    }
-
-    fun nextPage(page: Int) {
-        CoroutineScope(Dispatchers.IO).launch {
-            _liveData.value = getCharactersListByPageUsecase.execute(page)
-        }
-    }
+    val liveData: StateFlow<PagingData<CharacterModel>> = _liveData
 
     companion object {
         val Factory: ViewModelProvider.Factory = viewModelFactory {
             initializer {
-                val charRepo by lazy(LazyThreadSafetyMode.NONE) { CharacterRepoImpl(ApiFactory) }
-                val getCharactersListUsecase by lazy(LazyThreadSafetyMode.NONE) {
-                    GetCharactersListUsecase(
-                        charRepo
-                    )
-                }
-                val getCharactersListByPageUsecase by lazy(LazyThreadSafetyMode.NONE) {
-                    GetCharactersListByPageUsecase(
-                        charRepo
-                    )
-                }
-                CharactersAllViewModel(getCharactersListUsecase, getCharactersListByPageUsecase)
+                CharactersAllViewModel()
             }
         }
     }
